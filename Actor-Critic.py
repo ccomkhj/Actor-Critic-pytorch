b0e6abc2058594af313d1759325258c350656f4f
ccomkhj <ccomkhj@gmail.com>
2020-03-19 14:00:35 +0100

    Update Actor-Critic.py

--- Actor-Critic.py
+++ Actor-Critic.py
@@ -51,7 +51,7 @@ def compute_returns(next_value, rewards, masks, gamma=0.99):
     R = next_value
     returns = []
     for step in reversed(range(len(rewards))):
-        R = rewards[step] + gamma * R * masks[step]
+        R = rewards[step] + gamma * R * masks[step] # mask를 통해, 마지막 step(끝)이면 rewards가 그대로 next_value가 된다.
         returns.insert(0, R)
     return returns
 
@@ -76,13 +76,13 @@ def trainIters(actor, critic, n_iters):
             action = dist.sample()
             next_state, reward, done, _ = env.step(action.cpu().numpy())
 
-            log_prob = dist.log_prob(action).unsqueeze(0)
-            entropy += dist.entropy().mean()
+            log_prob = dist.log_prob(action).unsqueeze(0) # log(policy)
+            entropy += dist.entropy().mean() # policy * log(policy)
 
-            log_probs.append(log_prob)
-            values.append(value)
-            rewards.append(torch.tensor([reward], dtype=torch.float, device=device))
-            masks.append(torch.tensor([1-done], dtype=torch.float, device=device))
+            log_probs.append(log_prob) # sigma (log(policy))를 구하기 위한 list
+            values.append(value) # 각 critic의 value function을 구하기 위한 list
+            rewards.append(torch.tensor([reward], dtype=torch.float, device=device)) # 각 단계의 reward를 리스트에 추가, TD target을 계산하는데 필요.
+            masks.append(torch.tensor([1-done], dtype=torch.float, device=device)) # 1 minus 0, 즉 끝났는지 확인하기 위한 리스트
 
             state = next_state
 
@@ -92,17 +92,17 @@ def trainIters(actor, critic, n_iters):
 
 
         next_state = torch.FloatTensor(next_state).to(device)
-        next_value = critic(next_state)
-        returns = compute_returns(next_value, rewards, masks)
+        next_value = critic(next_state) # V(s_t+1)을 구하기 위함
+        returns = compute_returns(next_value, rewards, masks) # TD target을 구함. returns는 list이며 각 단계마다의 TD target이 표기됨.
 
-        log_probs = torch.cat(log_probs)
-        returns = torch.cat(returns).detach()
-        values = torch.cat(values)
+        log_probs = torch.cat(log_probs)  # 한 iteration에서 구한 log_probs을 붙임
+        returns = torch.cat(returns).detach()   # 한 iteration에서 구한 TD target을 붙임
+        values = torch.cat(values)        # 한 iteration에서 구한 value을 붙임
 
-        advantage = returns - values
+        advantage = returns - values    # TD Error
 
-        actor_loss = -(log_probs * advantage.detach()).mean()
-        critic_loss = advantage.pow(2).mean()
+        actor_loss = -(log_probs * advantage.detach()).mean()   # actor는 policy 이므로, log(TD error)를 loss로 계산. (앞에 -1을 통해 ascent)
+        critic_loss = advantage.pow(2).mean()    # critic은 TD error를 MSE 으로 loss로 계산.
 
         optimizerA.zero_grad()
         optimizerC.zero_grad()
